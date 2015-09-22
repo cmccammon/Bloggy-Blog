9bf1559ba45a5e3ddfd1cba14067a77f2b06f419
Chris McCammon <cmccammon@gmail.com>
2015-09-22 15:48:56 -0500

    working thru controllers. tested application html is working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,14 @@
 class PostsController < ApplicationController
-def index
-  render template: 'posts/index.html.erb', locals: { posts: Post.all}
+  def index
+    render template: 'posts/index.html.erb', locals: { posts: Post.all}
+  end
+
+  def show
+    render template: 'posts/show.html.erb', locals: { posts: Post.find(params[:id])}
+  end
+
+  def create
+
+
+  end
 end
-
-def show
-  render template: 'posts/show.html.erb', locals: { posts: Post.find(params[:id])}
-end
-
-def create
-
-end
-
