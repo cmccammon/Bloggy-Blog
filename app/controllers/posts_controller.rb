ac06c54ddfbbae2a0ace968ffa884242f8a56f00
Chris McCammon <cmccammon@gmail.com>
2015-09-22 15:03:51 -0500

    updating controllers, routes and tbles done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,13 @@
 class PostsController < ApplicationController
+def index
+  render template: 'posts/index.html.erb', locals: { posts: Post.all}
 end
+
+def show
+  render template: 'posts/show.html.erb', locals: { posts: Post.find(params[:id])}
+end
+
+def create
+
+end
+
