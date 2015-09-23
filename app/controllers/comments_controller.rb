3e19099a326ec50e439c122897442af3cabbd69c
Chris McCammon <cmccammon@gmail.com>
2015-09-22 21:54:56 -0500

    all requirements done except create comment in post show

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,19 @@
 class CommentsController < ApplicationController
 
+def show
+  render template: 'comments/show.html.erb', locals: { comment: Comment.find(params[:id])}
+end
 
+def new
+  render template: 'comments/new.html.erb', locals: { comment: Comment.new}
+end
+
+def create
+  comment = Comment.new
+    comment.message   = params.fetch(:comment).fetch(:message)
+    comment.save
+
+end
 
 
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,11 +4,23 @@ class PostsController < ApplicationController
   end
 
   def show
-
-    render template: 'posts/show.html.erb', locals: { posts: Post.find(params[:id])}
-
-
+    post_title = Post.all.map { |u| [u.title, u.id] }
+    render template: 'posts/show.html.erb', locals: { posts: Post.find(params[:id]), comment: Comment.new, post_title: post_title}
   end
 
+  def new
+    render template: 'posts/new.html.erb', locals: { posts: Post.new}
+  end
+  def create
+    post = Post.new
+      post.title   = params.fetch(:post).fetch(:title)
+      post.body   = params.fetch(:post).fetch(:body)
+      post.published = false
+      if post.save
+        redirect_to post_path(post)
+      else
+        redirect_to posts_path
+    end
+  end
 
 end
